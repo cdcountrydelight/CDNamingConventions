9ff90896ab5b42bd9c0e17615fa1863f740e5e2b
cd-user <arpitkatiyar@countrydelight.in>
2024-04-16 16:57:34 +0530

    updated library

--- LintRulesLibrary/build.gradle.kts
+++ LintRulesLibrary/build.gradle.kts
@@ -1,14 +1,17 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
     id("com.android.lint")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
     compileOnly("com.android.tools.lint:lint-api:31.2.2")
 }
+
+
