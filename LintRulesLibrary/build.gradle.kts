fa8c49c8df761cbb2300bcced9185c41f43df28e
Arpit Katiyar <arpitkatiyar@countrydelight.in>
2024-08-02 16:28:47 +0530

    updated versions

--- LintRulesLibrary/build.gradle.kts
+++ LintRulesLibrary/build.gradle.kts
@@ -23,7 +23,7 @@ afterEvaluate {
                 }
                 groupId = "com.github.cdcountrydelight"
                 artifactId = "CDNamingConventions"
-                version = "1.0.6"
+                version = "1.0.7"
                 from(components["java"])
             }
         }
